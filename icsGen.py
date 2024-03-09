cd72a8639b3f6e5b3e9986228fa0ec6cb6db80f0
Colboi <1016886725@qq.com>
2024-03-09 19:38:56 +0800

    current time added

--- icsGen.py
+++ icsGen.py
@@ -1,11 +1,13 @@
 import json
-import os
+from datetime import datetime
 
 JSON_FILE = 'events.json'
 
 events = None
 with open(JSON_FILE, 'r') as f:
     events = json.load(f)['events']
+
+print("current time:", datetime.now())
 
 with open('event.ics', 'w', encoding='utf-8') as f:
     f.write("""BEGIN:VCALENDAR

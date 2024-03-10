6866ace119e11459c965931e7823dd12924a7526
Colboi <1016886725@qq.com>
2024-03-11 03:23:56 +0800

    server raw view added

--- server/server.py
+++ server/server.py
@@ -8,9 +8,16 @@ app = Flask(__name__)
 FILES_DIRECTORY = '/home/ubuntu/ics-gen'
 
 @app.route('/contests', methods=['GET'])
-def get_mpd():
+def get_contests():
     ics_file_path = os.path.join(FILES_DIRECTORY, "schedule.ics")
     response = make_response(send_from_directory(directory=os.path.dirname(ics_file_path), 
                                                  path=os.path.basename(ics_file_path), 
                                                  as_attachment=True))
+    return response
+
+@app.route('/contests_raw', methods=['GET'])
+def get_contests_raw():
+    ics_file_path = os.path.join(FILES_DIRECTORY, "events.json")
+    response = make_response(send_from_directory(directory=os.path.dirname(ics_file_path), 
+                                                 path=os.path.basename(ics_file_path)))
     return response
